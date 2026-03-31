6fe9fa27a02542c6dcaf963d7e2f4ebe78cc8915
Masha <130176845+Mashalyk@users.noreply.github.com>
2026-03-31 21:14:57 +0200

    add multiple password generation

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import random
 import string
 
 length = int(input("Enter password length: "))
+count = int(input("How many passwords to generate: "))
 
 characters = string.ascii_letters + string.digits + string.punctuation
-password = ""
 
-for i in range(length):
-    password += random.choice(characters)
-
-print("Generated password:", password)
+for i in range(count):
+    password = ""
+    for j in range(length):
+        password += random.choice(characters)
+    print("Generated password:", password)
