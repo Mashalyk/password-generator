32539096802ef623162a32bef93a6d1266990904
Masha <130176845+Mashalyk@users.noreply.github.com>
2026-04-01 20:44:31 +0200

    add input validation

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import random
 import string
 
-length = int(input("Enter password length: "))
-count = int(input("How many passwords to generate: "))
+try:
+    length = int(input("Enter password length: "))
+    count = int(input("How many passwords to generate: "))
+except ValueError:
+    print("Please enter numbers only.")
+    exit()
 
 characters = string.ascii_letters + string.digits + string.punctuation
 
